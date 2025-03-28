95e97b9671a2d0d728dfc55d906ba3b3315987e5
caringallawrence <caringallawrence5@gmail.com>
2025-03-28 19:35:35 +1300

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -78,11 +78,11 @@ const Footer = () => {
             <ul className="footer-list contact-list">
               <li>
                 <span className="contact-icon">📍</span>
-                123 Main Street, Hamilton, New Zealand
+                312 Nawton, Hamilton, New Zealand
               </li>
               <li>
                 <span className="contact-icon">📱</span>
-                +64 123 456 789
+                022 124 8553
               </li>
               <li>
                 <span className="contact-icon">
@@ -90,7 +90,7 @@ const Footer = () => {
                     <path d="m18.73 5.41-1.28 1L12 10.46 6.55 6.37l-1.28-1A2 2 0 0 0 2 7.05v11.59A1.36 1.36 0 0 0 3.36 20h3.19v-7.72L12 16.37l5.45-4.09V20h3.19A1.36 1.36 0 0 0 22 18.64V7.05a2 2 0 0 0-3.27-1.64z"></path>
                   </svg>
                 </span>
-                info@artisantiling.co.nz
+                lawrencecaringal5@gmail.com
               </li>
               <li>
                 <span className="contact-icon">⏰</span>
